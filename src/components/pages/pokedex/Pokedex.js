89813eece5de28c147b8a4ed88d055d03dd28fab
danema21 <damianema21@gmail.com>
2022-05-26 14:20:01 -0300

    se avanza con el diseño de pokedex

--- src/components/pages/pokedex/Pokedex.js
+++ src/components/pages/pokedex/Pokedex.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Col, Container, Row, Button, Image } from "react-bootstrap";
 import http from "../../../http-common";
 import PokeAPIServices from "../../../services/PokeAPIServices";
 import "./pokedex.css";
@@ -52,26 +53,59 @@ const Pokedex = () => {
         })
     }
 
-    const getRandom = () => {
-        retrievePokemon(Math.round(Math.random()*801 + 1));
-    }
-
     const getPokemonOfTheDay = () => {
         const date = new Date();
-        const pokemonId = ((date.getFullYear() * date.getDate() * (date.getMonth()+1)) % 802) + 1;
+        const pokemonId = ((date.getFullYear() * date.getDate() * (date.getMonth()+1)) % 150) + 1;
         retrievePokemon(pokemonId);
     }
 
     const whosThatPokemonImg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
 
     return(
-        <div>
-            <button onClick={getRandom}>Random</button>
-            <h1 className="pokemon-name">{pokemon.name} #{pokemon.id}</h1>
-            <img src={pokemon.sprites.front_default} alt="sprite.png"/>
+        <Container>
+            <Row>
+                <Col lg={6}>
+                    {/*
+                    <div>
+                        <h1 className="pokemon-name">{pokemon.name} #{pokemon.id}</h1>
+                        <img src={pokemon.sprites.front_default} alt="sprite.png"/>
+                        
+                        <p>{description}</p>
+                    </div>
+                    */}
+                    <Row className="mb-4">
+                        <div className="search-form">
+                            <input type="text" placeholder="Search by name or ID..."/>
+                            <Button className="go-btn">Go</Button>
+                        </div>
+                    </Row>
+                    <Row className="mb-4">
+                        <Col sm={6}>
+                            <div className="left-right-btn">
+                                <div className="up-down-btn"></div>
+                            </div>
+                        </Col>
+                        <Col sm={6}>
+                            <div className="screen-layout">
+                                <div className="screen">
+                                    <Image src="" alt="sprite.png" className="sprite"/>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <h1>Description</h1>
+                        <p>Comienzo dedescription</p>
+                    </Row>
+                </Col>
+                <Col lg={6}>
+                    <h1 className="info">Nombre del pokemon</h1>
+                    <h2 className="info">Type</h2>
+                    <h2 className="info">Stats</h2>
+                </Col>
+            </Row>
             
-            <p>{description}</p>
-        </div>
+        </Container>
     );
 }
 
