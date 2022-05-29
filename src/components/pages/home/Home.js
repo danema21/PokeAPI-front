3f36d84a19db8ef815b8fb0b4bc718f494670034
danema21 <damianema21@gmail.com>
2022-05-28 23:37:29 -0300

    se implementa una animación de carga

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Container, Image, Row, Col, Button } from "react-bootstrap";
 import PokeAPIServices from "../../../services/PokeAPIServices";
+import LoadingSpinner from "../../loadingSpinner/LoadingSpinner";
 import "./home.css";
 
 const Home = () => {
@@ -8,16 +9,20 @@ const Home = () => {
 
     const [todayPokemon, setTodayPokemon] = useState({});
     const [hide, setHide] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         getPokemonOfTheDay();
     }, []);
 
     const retrievePokemon = (id) => {
+        setIsLoading(true);
         PokeAPIServices.get(id).then(response => {
             setTodayPokemon(response.data);
+            setIsLoading(false);
             console.log(response.data);
         }).catch(e => {
+            setIsLoading(true);
             console.log(e);
         });
     }
@@ -34,7 +39,7 @@ const Home = () => {
 
     return(
         <Container className="container" fluid>
-            {todayPokemon && 
+            {(isLoading === false) ?
                 <Row>
                     <Col xs={12} sm={3} md={4}>
                         {/*empty solo para centrar*/}
@@ -48,12 +53,14 @@ const Home = () => {
                         </div>
                     </Col>
                 </Row>
+                :
+                <LoadingSpinner/>
             }
+            
             <div className="guess-form">
                 {hide ? <h1>???</h1> : <h1>It's {todayPokemon.name} {"#" + todayPokemon.id}</h1>}
                 <Button className="guess-btn shadow" onClick={showPokemon}>who's that pokémon?</Button>
             </div>
-            
             
             <div className="footer fixed-bottom">©2022 Copyright Pokédex Tecnólogo en Informática</div>
         </Container>
